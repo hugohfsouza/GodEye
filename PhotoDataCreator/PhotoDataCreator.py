c2e948bce7d794128e2af5c6ebc893ee5c6d14ea
Hugo Fumero <hugohfsouza@gmail.com>
2021-05-15 21:38:55 -0300

    no message

--- PhotoDataCreator/PhotoDataCreator.py
+++ PhotoDataCreator/PhotoDataCreator.py
@@ -81,97 +81,92 @@ def analisarDiretorio(person):
     countAtual = countAtual + 1
 
 for person in train_dir:
-    print(person)
+    # print(person)
     if(person != ".DS_Store" and person != ".gitkeep" ):
         countEsperado = countEsperado + 1
 
 for person in train_dir:
-    # bar.next()
+    bar.next()
 
     if(person != ".DS_Store" and person != ".gitkeep" ):
         x = threading.Thread(target=analisarDiretorio, args=(person,))
         x.start()
 
-# bar.finish()
-
-while(countAtual != countEsperado):
-    print(str(countAtual) + "  " + str(countEsperado))
-    # time.sleep(1)
-
-print("todas as threads terminaram")
-
-# clf = svm.SVC(gamma='scale')
-# clf.fit(encodings,names)
+bar.finish()
 
 
-# app = Flask(__name__)
-# CORS(app)
-
-# @app.route('/getinfo', methods=['GET'])
-# def getInfo():
-
-#     id = request.args.get('id', default = -1, type = int)
-
-#     print(id)
-#     if(id == -1):
-#         return app.response_class(
-#             response=json.dumps({}),
-#             status=404,
-#             mimetype='application/json'
-#         )
-
-#     bancoDados2 = DBControl();
-#     data = bancoDados2.getInformations(id)
-
-#     data = {"nome": data[0], "link": data[1]}
-
-#     return app.response_class(
-#         response=json.dumps(data),
-#         status=200,
-#         mimetype='application/json'
-#     )
+clf = svm.SVC(gamma='scale')
+clf.fit(encodings,names)
 
 
-# @app.route('/reconhecer', methods=['POST'])
-# def reconhecer():
-#     y = json.loads(request.data)
+app = Flask(__name__)
+CORS(app)
 
-#     filename    = str(uuid.uuid4())+".png" 
+@app.route('/getinfo', methods=['GET'])
+def getInfo():
+
+    id = request.args.get('id', default = -1, type = int)
+
+    print(id)
+    if(id == -1):
+        return app.response_class(
+            response=json.dumps({}),
+            status=404,
+            mimetype='application/json'
+        )
+
+    bancoDados2 = DBControl();
+    data = bancoDados2.getInformations(id)
+
+    data = {"nome": data[0], "link": data[1]}
+
+    return app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+
+
+@app.route('/reconhecer', methods=['POST'])
+def reconhecer():
+    y = json.loads(request.data)
+
+    filename    = str(uuid.uuid4())+".png" 
     
-#     base64_data = re.sub('^data:image/.+;base64,', '', y['file'])
-#     byte_data = base64.b64decode(base64_data)
-#     image_data = BytesIO(byte_data)
-#     img = Image.open(image_data)
-#     img.save(filename, "PNG")
+    base64_data = re.sub('^data:image/.+;base64,', '', y['file'])
+    byte_data = base64.b64decode(base64_data)
+    image_data = BytesIO(byte_data)
+    img = Image.open(image_data)
+    img.save(filename, "PNG")
 
     
-#     imageSearch = face_recognition.load_image_file(filename)
+    imageSearch = face_recognition.load_image_file(filename)
 
-#     face_locations = face_recognition.face_locations(imageSearch)
-#     no = len(face_locations)
+    face_locations = face_recognition.face_locations(imageSearch)
+    no = len(face_locations)
 
-#     try:
-#         os.remove(filename)
-#     except:
-#         pass
+    try:
+        os.remove(filename)
+    except:
+        pass
 
  
-#     for i in range(no):
-#         test_image_enc = face_recognition.face_encodings(imageSearch)[i]
-#         idPerfil = clf.predict([test_image_enc])
-#         return app.response_class(
-#             response=json.dumps({"data": {"id":idPerfil[0]}}),
-#             status=200,
-#             mimetype='application/json'
-#         )
+    for i in range(no):
+        test_image_enc = face_recognition.face_encodings(imageSearch)[i]
+        idPerfil = clf.predict([test_image_enc])
+        return app.response_class(
+            response=json.dumps({"data": {"id":idPerfil[0]}}),
+            status=200,
+            mimetype='application/json'
+        )
     
-#     return app.response_class(
-#         response=json.dumps({}),
-#         status=404,
-#         mimetype='application/json'
-#     )
+    return app.response_class(
+        response=json.dumps({}),
+        status=404,
+        mimetype='application/json'
+    )
 
 
-# app.run(debug=True, port=5001)
+app.run(debug=True, port=5001)
 
 
